4620f6757d31c848b35194d178195b2a19df736f
Xiangnan-Wu <xiangnanwu.cs@gmail.com>
2025-09-16 16:46:52 +0800

    进行了代码规范化

--- debug.py
+++
@@ -1,8 +0,0 @@
-import keyboard
-
-while True:
-    g = keyboard.on_press_key('g', print)
-    r = keyboard.on_press_key('r', print)
-    keyboard.wait('esc')
-    keyboard.unhook(g)
-    keyboard.unhook(r)
